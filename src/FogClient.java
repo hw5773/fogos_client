47aa7e0bf5801ed0008f387c90b377cca6befcbc
Hyunwoo Lee <hw5773@gmail.com>
2020-10-21 22:18:52 -0400

    revise the errors

--- src/FogClient.java
+++ src/FogClient.java
@@ -13,6 +13,7 @@ import FogOSService.ServiceContext;
 import FogOSService.ServiceType;
 import sun.misc.Request;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.security.*;
@@ -26,7 +27,7 @@ public class FogClient {
     private static final String rootPath = "D:\\tmp";
 
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, SignatureException, InvalidKeyException, InvalidKeySpecException, SignatureException, InvalidKeyException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, SignatureException, InvalidKeyException, InvalidKeySpecException, SignatureException, InvalidKeyException, IOException, InterruptedException {
             // 1. Initialize the FogOSClient instance.
             // This will automatically build the contentStore inside the core,
             // a list of services, and a list of resources
@@ -34,7 +35,7 @@ public class FogClient {
 
             // 2. Add manually resource, content, or service
             // 2-1. Add resources to be monitored
-        /*
+
             Resource testResource = new Resource("Test", ResourceType.NetworkInterface,
                 "", "hop", false) {
                 @Override
@@ -43,47 +44,17 @@ public class FogClient {
                     System.out.println("[Resource Test] " + this.getCurr() + " " + this.getUnit());
                 }
             };
-         */
-
-        /*
-            Resource ifaceResource = new Resource("ifaceType", ResourceType.NetworkInterface,
-                    "", "Ethernet", false) {
-                @Override
-                public void monitorResource() {
-
-                }
-            };
-            Resource hwAddrResource = new Resource("hwAddress", ResourceType.NetworkInterface,
-                "", "20-12-A3-B2-8J-KU", false) {
-                @Override
-                public void monitorResource() {
-
-                }
-            };
-            Resource ipResource = new Resource("ipv4", ResourceType.NetworkInterface,
-                "", "1.2.3.4", false) {
-                @Override
-                public void monitorResource() {
-
-                }
-            };
-
-            fogos.addResource(ifaceResource);
-            fogos.addResource(hwAddrResource);
-            fogos.addResource(ipResource);
-
-         */
 
         // 2-2. Add content manually if any
             //Content testContent = new Content("test.txt", "C:\\Users\\HMLEE\\FogOS\\test.txt", true, "Hash");
-            Content testContent = new Content("test.jpg", "D:\\tmp\\test.jpg", true, "SHA1-HASH");
+            Content testContent = new Content("test.png", "D:\\tmp\\test.png", true, "SHA1-HASH");
             fogos.addContent(testContent);
 
             // 2-3. Add service to run
             KeyPairGenerator testServiceKeyPairGenerator = KeyPairGenerator.getInstance("RSA");
             testServiceKeyPairGenerator.initialize(2048);
             KeyPair testServiceKeyPair = testServiceKeyPairGenerator.genKeyPair();
-            Locator testServiceLoc = new Locator(InterfaceType.ETH, "127.0.0.1", 5550);
+            Locator testServiceLoc = new Locator(InterfaceType.ETH, "127.0.0.1", 5551);
             ServiceContext testServiceCtx = new ServiceContext("FogClientTestService",
                     ServiceType.Streaming, testServiceKeyPair, testServiceLoc, false, null);
             Service testService = new Service(testServiceCtx) {
@@ -131,7 +102,7 @@ public class FogClient {
             chosen = replyList.get(0).getFlexID();
 
             request = fogos.makeRequestMessage(chosen);
-            fogos.sendRequestMessage(request);
+            fogos.proxyRequestMessage(request);
 
             do {
                 response = fogos.getResponseMessage();
